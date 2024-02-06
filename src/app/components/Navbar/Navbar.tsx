e81a7e13a0833c6037aa38c319e14c853c16e3ea
tanvir <tanvirulislam149@gmail.com>
2024-02-06 22:45:03 +0600

    navbar done

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import logo from "../../../assets/Logo.png";
+import Image from "next/image";
+
+const Navbar = () => {
+	return (
+		<div className="navbar bg-base-100 md:px-10 py-3 sticky top-0">
+			<div className="navbar-start">
+				<Link href={"/"}>
+					<Image src={logo} width={200} height={200} alt="logo" />
+				</Link>
+			</div>
+			{/* mobile view */}
+			<div className="navbar-end">
+				<div className="dropdown dropdown-end">
+					<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>
+						</svg>
+					</div>
+					<ul
+						tabIndex={0}
+						className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40"
+					>
+						<li className="py-2">
+							<Link href={"/"}>Home</Link>
+						</li>
+						<li className="py-2">
+							<Link href={"/"}>Donations</Link>
+						</li>
+						<li className="py-2">
+							<Link href={"/"}>Statistics</Link>
+						</li>
+						<button className="btn btn-primary my-2">Login</button>
+					</ul>
+				</div>
+			</div>
+			<div className="navbar-end hidden lg:flex">
+				<ul className="menu menu-horizontal px-1 items-center">
+					<Link className="px-6" href={"/"}>
+						Home
+					</Link>
+					<Link className="px-6" href={"/"}>
+						Donations
+					</Link>
+					<Link className="px-6" href={"/"}>
+						Statistics
+					</Link>
+					<button className="btn btn-primary">Login</button>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main>
+		<main style={{ height: "500vh" }}>
 			<h1>Checking</h1>
 		</main>
 	);
