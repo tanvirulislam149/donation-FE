1ad2095d75512e9021c2ad33972ee19167ba7c4d
tanvir <tanvirulislam149@gmail.com>
2024-02-06 23:06:38 +0600

    login page done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className="flex justify-center items-center mt-20">
+			<div className="md:w-1/4 w-5/6">
+				<p className="text-center text-4xl font-bold mb-5">Login</p>
+				<div className="label">
+					<span className="label-text">Name:</span>
+				</div>
+				<input
+					type="text"
+					placeholder="Enter your name"
+					className="input input-bordered w-full"
+				/>
+				<div className="label">
+					<span className="label-text">Password:</span>
+				</div>
+				<input
+					type="password"
+					placeholder="Enter your password"
+					className="input input-bordered w-full"
+				/>
+				<button className="btn btn-primary mt-3">Login</button>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
