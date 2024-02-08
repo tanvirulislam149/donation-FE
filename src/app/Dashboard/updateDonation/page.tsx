2e2b805bb9cec930eae23345adc59e516a393143
tanvir <tanvirulislam149@gmail.com>
2024-02-08 20:26:38 +0600

    update done

--- src/app/Dashboard/updateDonation/page.tsx
+++ src/app/Dashboard/updateDonation/page.tsx
@@ -1,77 +1,82 @@
 "use client";
 
-import { ICreateDonation } from "@/types/globalTypes";
+import UpdateModal from "@/app/components/UpdateModal/UpdateModal";
+import { IDonation } from "@/types/globalTypes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const UpdateDonation = () => {
-	const handleUpdate = (e: React.FormEvent<ICreateDonation>) => {
-		console.log("object");
+	const [id, setId] = useState("");
+	const [data, setData] = useState([]);
+
+	const getDonation = () => {
+		axios(`http://localhost:5000/getAllDonation`)
+			.then(function (response) {
+				setData(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 	};
+	useEffect(() => {
+		getDonation();
+	}, []);
 	return (
-		<div className="flex justify-center w-full">
-			<form onSubmit={handleUpdate} className="w-1/2">
-				<p className="text-3xl text-center font-bold">Update Donation</p>
-				<label className="form-control w-full">
-					<div className="label">
-						<span className="label-text">Title:</span>
-					</div>
-					<input
-						name="titleName"
-						required
-						type="text"
-						placeholder="Enter title"
-						className="input input-bordered w-full"
-					/>
-				</label>
-				<label className="form-control w-full">
-					<div className="label">
-						<span className="label-text">Category:</span>
-					</div>
-					<input
-						name="donation_category"
-						required
-						type="text"
-						placeholder="Enter category"
-						className="input input-bordered w-full"
-					/>
-				</label>
-				<label className="form-control w-full">
-					<div className="label">
-						<span className="label-text">Description:</span>
-					</div>
-					<input
-						name="description"
-						required
-						type="text"
-						placeholder="Enter description"
-						className="input input-bordered w-full"
-					/>
-				</label>
-				<label className="form-control w-full">
-					<div className="label">
-						<span className="label-text">Picture:</span>
-					</div>
-					<input
-						name="picture_url"
-						required
-						type="text"
-						placeholder="Enter title"
-						className="input input-bordered w-full"
-					/>
-				</label>
-				<label className="form-control w-full">
-					<div className="label">
-						<span className="label-text">Donation Quantity:</span>
-					</div>
-					<input
-						name="money"
-						required
-						type="text"
-						placeholder="Enter title"
-						className="input input-bordered w-full"
-					/>
-				</label>
-				<input className="btn btn-primary mt-3" type="submit" value="Create" />
-			</form>
+		<div>
+			<div className="flex justify-center my-5">
+				<div className="overflow-x-auto w-3/4">
+					<p className="text-center text-3xl font-bold mb-5">My Donations</p>
+					{data.length ? (
+						<table className="table">
+							{/* head */}
+							<thead>
+								<tr className="text-black">
+									<th>#</th>
+									<th>title</th>
+									<th>Category</th>
+									<th>Donation</th>
+								</tr>
+							</thead>
+							<tbody>
+								{data.map((d: IDonation, index) => (
+									<tr key={d._id}>
+										<th>{index + 1}</th>
+										<td>{d.title}</td>
+										<td>{d.donation_category}</td>
+										<td>${d.money}</td>
+										<td>
+											<button
+												className="btn btn-sm"
+												onClick={() => {
+													setId(d._id);
+													(
+														document.getElementById(
+															"updateModal"
+														) as HTMLFormElement
+													)?.showModal();
+												}}
+											>
+												Update
+											</button>
+										</td>
+										<td>
+											<button
+												className="btn btn-sm btn-error"
+												onClick={() => setId(d._id)}
+											>
+												Delete
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					) : (
+						<p>No Data found</p>
+					)}
+				</div>
+			</div>
+			<UpdateModal getDonation={getDonation} id={id} />
 		</div>
 	);
 };
---
+++ src/app/components/UpdateModal/UpdateModal.tsx
@@ -0,0 +1,153 @@
+"use client";
+import { ICreateDonation, IDonation } from "@/types/globalTypes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const UpdateModal = ({
+	id,
+	getDonation,
+}: {
+	id: string;
+	getDonation: () => void;
+}) => {
+	const [data, setData] = useState<IDonation | null>(null);
+	const [title, setTitle] = useState(data?.title);
+	const [donation_category, setDonation_category] = useState(
+		data?.donation_category
+	);
+	const [description, setDescription] = useState(data?.description);
+	const [picture_url, setPicture_url] = useState(data?.picture_url);
+	const [money, setMoney] = useState(data?.money);
+
+	useEffect(() => {
+		axios(`http://localhost:5000/getDonation/${id}`)
+			.then(function (response) {
+				setData(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, [id]);
+
+	useEffect(() => {
+		setTitle(data?.title);
+		setDonation_category(data?.donation_category);
+		setDescription(data?.description);
+		setPicture_url(data?.picture_url);
+		setMoney(data?.money);
+	}, [data]);
+
+	const handleUpdate = (e: React.FormEvent<ICreateDonation>) => {
+		e.preventDefault();
+		const data = { title, donation_category, description, picture_url, money };
+		axios
+			.put(`http://localhost:5000/updateDonation/${id}`, data)
+			.then(function (response) {
+				if (response.data.acknowledged) {
+					setData(null);
+					(document.getElementById("updateModal") as HTMLFormElement)?.close();
+					getDonation();
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	};
+
+	return (
+		<dialog id="updateModal" className="modal">
+			<div className="modal-box">
+				<form method="dialog">
+					<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+						✕
+					</button>
+				</form>
+				{data ? (
+					<div className="flex justify-center w-full">
+						<form onSubmit={handleUpdate} className="w-full">
+							<p className="text-3xl text-center font-bold">Update Donation</p>
+							<label className="form-control w-full">
+								<div className="label">
+									<span className="label-text">Title:</span>
+								</div>
+								<input
+									name="titleName"
+									required
+									value={title}
+									onChange={(e) => setTitle(e.target.value)}
+									type="text"
+									placeholder="Enter title"
+									className="input input-bordered w-full"
+								/>
+							</label>
+							<label className="form-control w-full">
+								<div className="label">
+									<span className="label-text">Category:</span>
+								</div>
+								<input
+									name="donation_category"
+									required
+									value={donation_category}
+									onChange={(e) => setDonation_category(e.target.value)}
+									type="text"
+									placeholder="Enter category"
+									className="input input-bordered w-full"
+								/>
+							</label>
+							<label className="form-control w-full">
+								<div className="label">
+									<span className="label-text">Description:</span>
+								</div>
+								<textarea
+									name="description"
+									required
+									value={description}
+									onChange={(e) => setDescription(e.target.value)}
+									placeholder="Enter description"
+									className="input input-bordered w-full"
+								/>
+							</label>
+							<label className="form-control w-full">
+								<div className="label">
+									<span className="label-text">Picture URL:</span>
+								</div>
+								<input
+									name="picture_url"
+									required
+									value={picture_url}
+									onChange={(e) => setPicture_url(e.target.value)}
+									type="text"
+									placeholder="Enter title"
+									className="input input-bordered w-full"
+								/>
+							</label>
+							<label className="form-control w-full">
+								<div className="label">
+									<span className="label-text">Donation Quantity:</span>
+								</div>
+								<input
+									name="money"
+									required
+									value={money}
+									onChange={(e) => setMoney(parseInt(e.target.value))}
+									type="text"
+									placeholder="Enter title"
+									className="input input-bordered w-full"
+								/>
+							</label>
+							<input
+								className="btn btn-primary mt-3"
+								type="submit"
+								value="Update"
+							/>
+						</form>
+					</div>
+				) : (
+					<p>Loading...</p>
+				)}
+			</div>
+		</dialog>
+	);
+};
+
+export default UpdateModal;
