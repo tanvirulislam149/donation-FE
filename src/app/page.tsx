4f2742182661072596c31a59799069aef3eaf0f4
tanvir <tanvirulislam149@gmail.com>
2024-03-02 01:00:13 +0600

    ssg checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import HomeContainer from "./components/HomeContainer";
 
 const getData = async () => {
-	const res = await fetch("https://donation-be.onrender.com/getAllDonation", {
-		cache: "no-store",
-	});
+	const res = await fetch("https://donation-be.onrender.com/getAllDonation");
 	return res.json();
 };
 
