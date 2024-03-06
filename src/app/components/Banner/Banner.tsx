37df889f0b5a16fcb88f7ce2e92bd4761f3853a5
tanvir <tanvirulislam149@gmail.com>
2024-03-06 16:43:12 +0600

    banner done

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -31,7 +31,7 @@ const Banner = ({
 		},
 	};
 	return (
-		<div className={`h-screen flex justify-center`}>
+		<div>
 			<Swiper
 				spaceBetween={30}
 				effect={"fade"}
@@ -49,12 +49,35 @@ const Banner = ({
 						className={`w-full`}
 						src="https://i.ibb.co/WWdyyps/larm-rmah-AEa-TUnvneik-unsplash.jpg"
 					/>
+					<div className={styles.bannerText}>
+						<p>
+							Home is <span className="text-green-400">Life</span>
+						</p>
+						<div className="text-white text-3xl font-normal mt-8">
+							<p>We build strength, stability and self reliance</p>
+							<p>through shelter</p>
+						</div>
+						<button className="btn btn-primary my-3 rounded-none bg-green-400 border-none text-black text-base font-bold hover:bg-green-700 md:m-0">
+							Donate
+						</button>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className={styles.bannerImg}>
 					<img
 						className={`w-full`}
 						src="https://i.ibb.co/G9CCsz0/un0381303.jpg"
 					/>
+					<div className={styles.bannerText}>
+						<p className={styles.firstText}>Children Needs</p>
+						<p className="text-green-400">Your Help</p>
+						<div className="text-white text-3xl font-normal mt-12">
+							<p>We build strength, stability and self reliance</p>
+							<p>through shelter</p>
+						</div>
+						<button className="btn btn-primary my-3 rounded-none bg-green-400 border-none text-black text-base font-bold hover:bg-green-700 md:m-0">
+							Donate
+						</button>
+					</div>
 				</SwiperSlide>
 			</Swiper>
 		</div>
