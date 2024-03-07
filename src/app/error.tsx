a7a662411b632183275d5d251d5a64d82a1262d0
tanvir <tanvirulislam149@gmail.com>
2024-03-07 16:27:27 +0600

    error page done

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const error = () => {
+	return (
+		<div>
+			<p className="text-3xl text-center my-20 font-bold">
+				Something went wrong. Please wait and refresh the page.{" "}
+			</p>
+		</div>
+	);
+};
+
+export default error;
